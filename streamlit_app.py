2a5cbd414acf75d561fda300efe708dd2ac2cb35
catahd04 <111128670+catahd04@users.noreply.github.com>
2025-02-04 12:18:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,9 @@ from snowflake.snowpark.functions import col
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response.json())
+
+
 # Write directly to the app
 
 st.title("My parents new healthy dinner!! ")
